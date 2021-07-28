f6f7424744ba816d1e898fbcc215b15a8c8308db
jrflorentine <53338319+jrflorentine@users.noreply.github.com>
2021-07-28 13:18:59 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,6 @@ let message_string = "I'm-a-visitor-from-Planet-Titus."
 basic.forever(function () {
     for (let index = 0; index <= message_string.length; index++) {
         radio.setGroup(index)
-        radio.sendString("" + (index))
+        radio.sendString(message_string.charAt(index))
     }
 })
