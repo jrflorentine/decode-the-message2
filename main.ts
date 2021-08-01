a20b283b03acecf5199f598345ac67292cc54b8a
jrflorentine <53338319+jrflorentine@users.noreply.github.com>
2021-08-01 14:51:33 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
-let message_string = "I'm-a-visitor-from-Planet-Titus."
+let message_string = "I'm a student from Planet Titus. I'm visiting your planet for a school assignment."
+let numStudents = 10
 basic.forever(function () {
-    for (let index = 0; index <= message_string.length; index++) {
+    for (let index = 0; index <= numStudents - 1; index++) {
         radio.setGroup(index)
-        radio.sendString(message_string.charAt(index))
+        if (message_string.charAt(index) == " ") {
+            radio.sendString("-")
+            basic.showString("-")
+        } else {
+            radio.sendString(message_string.charAt(index))
+            basic.showString(message_string.charAt(index))
+        }
     }
 })
