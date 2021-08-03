9553216d23889c609ee2d7854a238445b34f47d8
jrflorentine <53338319+jrflorentine@users.noreply.github.com>
2021-08-03 15:19:29 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,8 @@
-let short_message = "I'm a student"
-let long_message = "I'm a student from Planet Titus"
-let current_message = short_message
+let message = "I'm-a-student"
 basic.forever(function () {
-    for (let index = 0; index <= current_message.length - 1; index++) {
+    for (let index = 0; index <= message.length - 1; index++) {
         radio.setGroup(index)
-        if (current_message.charAt(index) == " ") {
-            radio.sendString("-")
-            basic.showString("-")
-        } else {
-            radio.sendString(current_message.charAt(index))
-            basic.showString(current_message.charAt(index))
-        }
+        radio.sendString(message.charAt(index))
+        basic.showString(message.charAt(index))
     }
 })
