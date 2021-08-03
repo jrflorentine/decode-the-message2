c7f10e11fea12942283cd6b51ec8b7f1b7849da5
jrflorentine <53338319+jrflorentine@users.noreply.github.com>
2021-08-03 15:17:18 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,15 @@
-let message_string = "I'm a student from Planet Titus. I'm visiting your planet for a school assignment."
-let numStudents = 10
+let short_message = "I'm a student"
+let long_message = "I'm a student from Planet Titus"
+let current_message = short_message
 basic.forever(function () {
-    for (let index = 0; index <= numStudents - 1; index++) {
+    for (let index = 0; index <= current_message.length - 1; index++) {
         radio.setGroup(index)
-        if (message_string.charAt(index) == " ") {
+        if (current_message.charAt(index) == " ") {
             radio.sendString("-")
             basic.showString("-")
         } else {
-            radio.sendString(message_string.charAt(index))
-            basic.showString(message_string.charAt(index))
+            radio.sendString(current_message.charAt(index))
+            basic.showString(current_message.charAt(index))
         }
     }
 })
